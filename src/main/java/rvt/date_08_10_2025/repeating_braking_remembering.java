6a6edb571c3eef7c9443d75c2c0cb2ba9c18bf9f
RVT-MZ <a240194mz@rvt.lv>
2025-10-08 15:25:57 +0300

    part 5

--- src/main/java/rvt/date_08_10_2025/repeating_braking_remembering.java
+++ src/main/java/rvt/date_08_10_2025/repeating_braking_remembering.java
@@ -8,11 +8,17 @@ public class repeating_braking_remembering {
         System.out.println("Dod numurus:");
         int sum = 0;
         int quat = 0;
+        int para = 0;
+        int nepara = 0;
         while (true) {
             int num = Integer.valueOf(scan.nextLine());
             if (num == -1) {
                 System.out.println("Plds! Ata!");
                 break;
+            } else if (num % 2 == 0) {
+                para++;
+            } else {
+                nepara++;
             }
             sum = sum + num;
             quat++;
@@ -20,6 +26,7 @@ public class repeating_braking_remembering {
         System.out.println("Summa ir " + sum);
         System.out.println("Reizes bija: " + quat);
         System.out.println("Videjais ir: " + (sum / (double) quat));
-        
+        System.out.println("Para: " + para);
+        System.out.println("Nepara: " + nepara);
     }
 }
